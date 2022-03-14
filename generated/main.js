371a3cfad63575cc59b1c7135b11dca8ce3c4949
Josh Gearhart <joshua.d.gearhart@gmail.com>
2022-03-14 08:37:04 -0400

    Inferring types

--- generated/main.js
+++ generated/main.js
@@ -1,28 +1,26 @@
-function logIt(x: number | string) {
+"use strict";
+function logIt(x) {
     if (typeof x === 'number') {
         logNumber(x);
-    } else {
+    }
+    else {
         logString(x);
     }
 }
-function logNumber(n: number) {
+function logNumber(n) {
     console.log(n);
 }
-function logString(s: string) {
+function logString(s) {
     console.log(s);
 }
-
 const hello = 'Hello, world';
 logString(hello);
-
 const x = 4;
 logNumber(5);
-
-let chimera: number | string;
+let chimera;
 chimera = 6;
 logNumber(chimera);
 chimera = 'hey, this is neat';
 logString(chimera);
-
 logIt(200);
 logIt('New msg');
